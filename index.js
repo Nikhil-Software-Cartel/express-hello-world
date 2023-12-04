10d10e67a2327e204a5fc9df7d1161c9d7b7e07c
M Nikhil Swami <nikhilswami1@gmail.com>
2023-12-05 01:01:02 +0530

    add wait

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+let worker_name = Math.random().toString(36).substring(7);
 app.get('/', function (req, res) {
-  res.send('Hello From Express!')
-})
+    // wait for 100 ms
+    setTimeout(function() {
+        res.send('Hello From Express!');
+        console.log(worker_name);
+    }, 100);
+
+    console.log(worker_name);
+});
 app.listen(3000, function () {
-  console.log('Listening on port 3000...')
-})
+    console.log('Listening on port 3000...');
+});
