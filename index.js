3a7efc917958e5780d05958fe59a773c613cb888
M Nikhil Swami <nikhilswami1@gmail.com>
2023-12-05 01:05:39 +0530

    remove duplicate msg

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ var app = express();
 let worker_name = Math.random().toString(36).substring(7);
 app.get('/', function (req, res) {
     // wait for 100 ms
-    setTimeout(function() {
-        res.send('Hello From Express!');
+    setTimeout(function () {
         console.log(worker_name);
+        res.send('Hello From Express!');
     }, 100);
-
-    console.log(worker_name);
 });
 app.listen(3000, function () {
     console.log('Listening on port 3000...');
