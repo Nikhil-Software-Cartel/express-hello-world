230b17e75a861a7c878fddafb08270f79b9145ed
Nikhil-Swami <54004431+Nikhil-Software-Cartel@users.noreply.github.com>
2023-12-05 00:23:39 +0530

    Update index.js hello message

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var express = require('express')
 var app = express()
-
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello From Express!')
 })
-
 app.listen(3000, function () {
   console.log('Listening on port 3000...')
 })
